e106357e63e4e2002a2f23b3f9d2d05ca0aa653d
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2024-10-08 11:30:25 +0530

    Yet another brocken keyboard 1200 rating

--- codeforcesQuestions/YetAnotherBrockenKey.cpp
+++ codeforcesQuestions/YetAnotherBrockenKey.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 #define ll long long
 void solve(){
-
+int n, k;
+cin>>n>>k;
+string st;
+cin>>st;
+set<ll>s;
+for(int i=0; i<k; i++){
+char c;
+cin>>c;
+s.insert(c);
+}
+ll ans = 0;
+for(int i=0; i<n; i++){
+    ll j = i;
+    while(j<n && s.find(st[j]) != s.end()){
+        j++;
+    }
+    ll len = j-i;
+    ans += len * 1ll * (len + 1)/2;
+    i = j;
+}
+cout<<ans<<endl;
 }
 int main(){
     ios::sync_with_stdio(0);
