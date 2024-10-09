b91c954a607246a25cc3285c545038256c491670
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2024-10-09 17:39:38 +0530

    1200 rating questions

--- codeforcesQuestions/ReplaceAndKeepSorted.cpp
+++ codeforcesQuestions/ReplaceAndKeepSorted.cpp
@@ -2,16 +2,28 @@
 using namespace std;
 #define ll long long
 void solve(){
+ll n,q,k;
+cin>>n>>q>>k;
+vector<ll>v(n);
+for(int i=0; i<n; i++){
+    cin>>v[i];
+}
 
+
+while(q--){
+    int l, r;
+    cin>>l>>r;
+ cout<<k + (v[r-1] - v[l-1] + 1) - 2*1ll*(r - l + 1)<<endl;
+}
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int t;
-    cin>>t;
-    while(t--){
+    // int t;
+    // cin>>t;
+    // while(t--){
     solve();
-    }
+    // }
     
     return 0;
 }
